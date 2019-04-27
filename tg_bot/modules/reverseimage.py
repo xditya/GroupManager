a8b500df855c802fe6fa0b6b9c886dd621b3f0ab
Mizukito Akito <peak@echln.net>
2019-04-27 16:49:25 +0000

    [ReverseImage] Image lookup :P
    Signed-off-by: Mizukito Akito <peak@echln.net>

--- tg_bot/modules/reverseimage.py
+++ tg_bot/modules/reverseimage.py
@@ -181,7 +181,8 @@ __help__ = """
 - /reverse: Does a reverse image search of the media which it was replied to.
 """
 
-__mod_name__ = "Lookup"
+__mod_name__ = "Image Lookup"
+
 REVERSE_HANDLER = DisableAbleCommandHandler("reverse", reverse, pass_args=True, admin_ok=True)
 
 dispatcher.add_handler(REVERSE_HANDLER)
