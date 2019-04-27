57af9b9c9327eada2c211e47e4a7746ba6f81130
IAmWolf94 <iamwolf94@github.com>
2019-04-27 17:38:08 +0000

    [Languages & General] Added Italian and some another editing
    Signed-off-by: Mizukito Akito <peak@echln.net>

--- tg_bot/modules/translations/strings.py
+++ tg_bot/modules/translations/strings.py
@@ -5,6 +5,7 @@ from tg_bot.modules.translations.Ukraine import UkrainianStrings
 from tg_bot.modules.translations.Spanish import SpanishStrings
 from tg_bot.modules.translations.Turkish import TurkishStrings
 from tg_bot.modules.translations.Indonesian import IndonesianStrings
+from tg_bot.modules.translations.Italian import ItalianStrings
 
 def tld(chat_id, t, show_none=True):
     LANGUAGE = prev_locale(chat_id)
@@ -20,6 +21,8 @@ def tld(chat_id, t, show_none=True):
         elif LOCALE in ('tr') and t in TurkishStrings:
             return TurkishStrings[t]
         elif LOCALE in ('id') and t in IndonesianStrings:
+            return IndonesianStrings[t]
+        elif LOCALE in ('it') and t in ItalianStrings:
             return IndonesianStrings[t]
         else:
             if t in EnglishStrings:
@@ -53,6 +56,8 @@ def tld_help(chat_id, t):
         elif LOCALE in ('tr') and t in TurkishStrings:
             return TurkishStrings[t]
         elif LOCALE in ('id') and t in IndonesianStrings:
+            return IndonesianStrings[t]
+        elif LOCALE in ('it') and t in ItalianStrings:
             return IndonesianStrings[t]
         else:
             return False
