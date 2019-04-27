a13c2802d60a5983d975c170af8fe3ad784fb620
Mizukito Akito <peak@echln.net>
2019-04-27 13:17:41 +0000

    [Language] Fixed some string
    Signed-off-by: Mizukito Akito <peak@echln.net>

--- tg_bot/modules/translations/Turkish.py
+++ tg_bot/modules/translations/Turkish.py
@@ -501,7 +501,7 @@ Available commands are:
  - /del: deletes the message you replied to. """,
 
 
-"Feds_help": """Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
+"Federations_help": """Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
 But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
 Inspired by [Rose bot](t.me/MissRose_bot)
 No more! With federations, you can make a ban in one chat overlap to all your other chats.
