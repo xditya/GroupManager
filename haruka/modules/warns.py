370be24e88adacfd24de171833f3129c881e77b0
アキト ミズキト <peak@echln.net>
2019-05-20 05:02:48 +0900

    [Warns] Fixed string
    Signed-off-by: アキト ミズキト <peak@echln.net>

--- haruka/modules/warns.py
+++ haruka/modules/warns.py
@@ -65,7 +65,7 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
         keyboard = InlineKeyboardMarkup(
             [[InlineKeyboardButton("Remove warn (admin only)", callback_data="rm_warn({})".format(user.id))]])
 
-        reply = "{} <b>has been warned!</b>\nTotal count of warn this user have :{}/{}".format(mention_html(user.id, user.first_name), num_warns,
+        reply = "{} <b>has been warned!</b>\nTotal warn that this user have {}/{}".format(mention_html(user.id, user.first_name), num_warns,
                                                              limit)
         if reason:
             reply += "\nReason for the warn:\n<code>{}</code>".format(html.escape(reason))
