fc1d99d34c7f0264bd86da6c5ca1a870e25e09fa
1maverick1 <maver_ck@outlook.com>
2019-06-02 16:14:54 +0900

    [Disable] Fix-up
    Signed-off-by: アキト ミズキト <peak@echln.net>

--- haruka/modules/disable.py
+++ haruka/modules/disable.py
@@ -9,10 +9,6 @@ from haruka import dispatcher
 from haruka.modules.helper_funcs.handlers import CMD_STARTERS
 from haruka.modules.helper_funcs.misc import is_module_loaded
 
-from haruka.modules.translations.strings import tld
-
-from haruka.modules.connection import connected
-
 FILENAME = __name__.rsplit(".", 1)[-1]
 
 # If module is due to be loaded, then setup all the magical handlers
@@ -72,113 +68,70 @@ if is_module_loaded(FILENAME):
     @user_admin
     def disable(bot: Bot, update: Update, args: List[str]):
         chat = update.effective_chat  # type: Optional[Chat]
-        user = update.effective_user  # type: Optional[User]
-    
-        conn = connected(bot, update, chat, user.id)
-        if not conn == False:
-            chatD = dispatcher.bot.getChat(conn)
-        else:
-            if chat.type == "private":
-                exit(1)
-            else:
-                chatD = update.effective_chat
-
         if len(args) >= 1:
             disable_cmd = args[0]
             if disable_cmd.startswith(CMD_STARTERS):
                 disable_cmd = disable_cmd[1:]
 
             if disable_cmd in set(DISABLE_CMDS + DISABLE_OTHER):
-                sql.disable_command(chatD.id, disable_cmd)
-                update.effective_message.reply_text(tld(chat.id, "Disabled the use of `{}` in *{}*").format(disable_cmd, chatD.title),
+                sql.disable_command(chat.id, disable_cmd)
+                update.effective_message.reply_text("Disabled the use of `{}`".format(disable_cmd),
                                                     parse_mode=ParseMode.MARKDOWN)
             else:
-                update.effective_message.reply_text(tld(chat.id, "That command can't be disabled"))
+                update.effective_message.reply_text("That command can't be disabled")
 
         else:
-            update.effective_message.reply_text(tld(chat.id, "What should I disable?"))
+            update.effective_message.reply_text("What should I disable?")
 
 
     @run_async
     @user_admin
     def enable(bot: Bot, update: Update, args: List[str]):
         chat = update.effective_chat  # type: Optional[Chat]
-        user = update.effective_user  # type: Optional[User]
-    
-        conn = connected(bot, update, chat, user.id)
-        if not conn == False:
-            chatD = dispatcher.bot.getChat(conn)
-        else:
-            if chat.type == "private":
-                exit(1)
-            else:
-                chatD = update.effective_chat
-
         if len(args) >= 1:
             enable_cmd = args[0]
             if enable_cmd.startswith(CMD_STARTERS):
                 enable_cmd = enable_cmd[1:]
 
-            if sql.enable_command(chatD.id, enable_cmd):
-                update.effective_message.reply_text(tld(chat.id, "Enabled the use of `{}` in *{}*").format(enable_cmd, chatD.title),
+            if sql.enable_command(chat.id, enable_cmd):
+                update.effective_message.reply_text("Enabled the use of `{}`".format(enable_cmd),
                                                     parse_mode=ParseMode.MARKDOWN)
             else:
-                update.effective_message.reply_text(tld(chat.id, "Is that even disabled?"))
+                update.effective_message.reply_text("Is that even disabled?")
 
         else:
-            update.effective_message.reply_text(tld(chat.id, "What should I enable?"))
+            update.effective_message.reply_text("What should I enable?")
 
 
     @run_async
     @user_admin
     def list_cmds(bot: Bot, update: Update):
-        chat = update.effective_chat  # type: Optional[Chat]
         if DISABLE_CMDS + DISABLE_OTHER:
             result = ""
             for cmd in set(DISABLE_CMDS + DISABLE_OTHER):
-                result += " • `{}`\n".format(escape_markdown(cmd))
-            update.effective_message.reply_text(tld(chat.id, "The following commands are toggleable:\n{}").format(result),
+                result += " - `{}`\n".format(escape_markdown(cmd))
+            update.effective_message.reply_text("The following commands are toggleable:\n{}".format(result),
                                                 parse_mode=ParseMode.MARKDOWN)
         else:
-            update.effective_message.reply_text(tld(chat.id, "No commands can be disabled."))
+            update.effective_message.reply_text("No commands can be disabled.")
 
 
     # do not async
-    def build_curr_disabled(chatD_id, chat_id):
-
-        disabled = sql.get_all_disabled(chatD_id)
+    def build_curr_disabled(chat_id: Union[str, int]) -> str:
+        disabled = sql.get_all_disabled(chat_id)
+        if not disabled:
+            return "No commands are disabled!"
 
         result = ""
         for cmd in disabled:
-            result += " • `{}`\n".format(escape_markdown(cmd))
-
-        if result == "":
-            return tld(chat_id, "No commands are disabled!")
-        else:
-            return result
+            result += " - `{}`\n".format(escape_markdown(cmd))
+        return "The following commands are currently restricted:\n{}".format(result)
 
 
     @run_async
     def commands(bot: Bot, update: Update):
         chat = update.effective_chat
-        user = update.effective_user  # type: Optional[User]
-    
-        conn = connected(bot, update, chat, user.id, need_admin=False)
-        if not conn == False:
-            chatD = dispatcher.bot.getChat(conn)
-        else:
-            if chat.type == "private":
-                exit(1)
-            else:
-                chatD = update.effective_chat
-
-        disabled = sql.get_all_disabled(chatD.id)
-        if not disabled:
-            update.effective_message.reply_text(tld(chat.id, "No commands are disabled! in *{}*!").format(chatD.title))
-
-        text = build_curr_disabled(chatD.id, chat.id)
-
-        update.effective_message.reply_text(tld(chat.id, "The following commands are currently restricted in *{}*:\n{}").format(chatD.title, text), parse_mode=ParseMode.MARKDOWN)
+        update.effective_message.reply_text(build_curr_disabled(chat.id), parse_mode=ParseMode.MARKDOWN)
 
 
     def __stats__():
@@ -189,32 +142,31 @@ if is_module_loaded(FILENAME):
         sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-    def __chat_settings__(bot, update, chat, chatP, user):
-        return build_curr_disabled(chat.id, chat.id)
+    def __chat_settings__(chat_id, user_id):
+        return build_curr_disabled(chat_id)
 
 
     __mod_name__ = "Command disabling"
 
     __help__ = """
-Not everyone wants every feature that rose offers. Some commands are best left unused; to avoid spam and abuse.
+Not everyone wants every feature that the bot offers. Some commands are best \
+left unused; to avoid spam and abuse.
 
-This allows you to disable some commonly used commands, so noone can use them. It'll also allow you to autodelete them, stopping people from
+This allows you to disable some commonly used commands, so noone can use them. \
+It'll also allow you to autodelete them, stopping people from bluetexting.
 
-Available commands are:
- - /disable <commandname>: stop users from using the "commandname" command in this group.
- - /enable <commandname>: allow users to use the "commandname" command in this group again.
- - /listcmds: list all disableable commands.
- - /disabled: list the disabled commands in this chat.
+ - /cmds: check the current status of disabled commands
 
-Note:
-When disabling a command, the command only gets disabled for non-admins. All admins can still use those commands.
-Disabled commands are still accessible through the /connect feature. If you would be interested to see this disabled too, let me know in the support chat.
+*Admin only:*
+ - /enable <cmd name>: enable that command
+ - /disable <cmd name>: disable that command
+ - /listcmds: list all possible toggleable commands
     """
 
-    DISABLE_HANDLER = CommandHandler("disable", disable, pass_args=True)
-    ENABLE_HANDLER = CommandHandler("enable", enable, pass_args=True)
-    COMMANDS_HANDLER = CommandHandler(["cmds", "disabled"], commands)
-    TOGGLE_HANDLER = CommandHandler("listcmds", list_cmds)
+    DISABLE_HANDLER = CommandHandler("disable", disable, pass_args=True, filters=Filters.group)
+    ENABLE_HANDLER = CommandHandler("enable", enable, pass_args=True, filters=Filters.group)
+    COMMANDS_HANDLER = CommandHandler(["cmds", "disabled"], commands, filters=Filters.group)
+    TOGGLE_HANDLER = CommandHandler("listcmds", list_cmds, filters=Filters.group)
 
     dispatcher.add_handler(DISABLE_HANDLER)
     dispatcher.add_handler(ENABLE_HANDLER)
