0bacb00a42676216843bc9f98a7d59114ca17f09
NikhilgowdaNY <63290784+NikhilgowdaNY@users.noreply.github.com>
2020-06-15 09:52:35 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -154,10 +154,13 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
-    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
-        InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
+    keyboard = [[InlineKeyboardButton(text="📢 Support Channel", url="https://t.me/ALL_MOVIES_LIABRARY")]]
 
+    keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
+
+    keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
+
+        InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
