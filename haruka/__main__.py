57cca2f75ef22f76d0b5141f724714e5d5352f21
pavansreyanth900 <76213600+pavansreyanth900@users.noreply.github.com>
2020-12-18 20:16:24 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,25 +22,16 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
+You know how hard it is sometimes to manage group
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group manager bot of @FILMY_ROCKERZZ_IN 
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
+A bot by @Featureswag_009
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
-"""
+ 
 
 
 IMPORTED = {}
