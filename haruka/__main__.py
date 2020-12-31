ad2dcac6cb9387463b13e522e9a4f2910bda97a7
balasarathi <75534391+balasarathi@users.noreply.github.com>
2020-12-31 13:15:44 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,17 +29,17 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [balasarathi 🇮🇳](t.me/balasarathi)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to [this person](t.me/subasri20) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
+Deploy a bot like me [now](www.github.com/balasarathi/vickybot)
 
 Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/gamee_gamestelgrambot?startgroup=true).
 """
 
 
