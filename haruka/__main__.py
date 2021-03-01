400c8f196422feb52b3d297403ded885148b2a94
P-Phreak <77227205+P-Phreak@users.noreply.github.com>
2021-03-01 13:17:55 +0530

    wizard.py
    Changed owner and site of Owner

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,11 +29,11 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [Predator](t.me/PredatorHackerzZ)
 
 Special thanks to [this person](t.me/denver02) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
+Deploy a bot like me [now](www.github.com/PredatorHackerzZ/Morgan Stark)
 
 Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
